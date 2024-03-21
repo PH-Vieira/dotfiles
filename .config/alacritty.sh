e3ed451d0585cfd61b72583d3ad1d43aa291701b
PH-Vieira <54160839+PH-Vieira@users.noreply.github.com>
2024-03-20 21:20:52 -0300

    other stuff config

---
+++ .config/alacritty.sh
@@ -0,0 +1,3 @@
+chsh -s /usr/bin/zsh
+img_path=$(cat ~/.cache/wal/wal)
+wal -i $img_path -n
---
+++ .config/random_wallpaper.sh
@@ -0,0 +1,3 @@
+# nitrogen --set-zoom-fill $img_path --save
+wal -i ~/.config/wallpapers/
+#img_path=$(cat ~/.cache/wal/wal)
